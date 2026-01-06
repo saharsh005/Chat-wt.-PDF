2b855fa9c7576e250f8e6524df78425be58b432d
SaharshDudhyal05 <saharshdudhyal@gmail.com>
2026-01-06 11:14:17 +0530

    Restructure project into root-level monorepo

--- queue/pdfQueue.js
+++
@@ -1,9 +0,0 @@
-import { Queue } from "bullmq";
-
-export const pdfQueue = new Queue("pdf-queue", {
-  connection: {
-    host: "localhost",
-    port: 6379,
-    maxRetriesPerRequest: null
-  }
-});
--- rag/splitter.js
+++
@@ -1,6 +0,0 @@
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-
-export const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 50
-});
